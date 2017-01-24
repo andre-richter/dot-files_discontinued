ce6877974e55af2e7f9a01e49f1bcb90129dd594
Andre Richter <andre.o.richter@gmail.com>
2017-01-24 15:53:29 +0100

    move agent conf

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 ln -s ~/repos/dot-files/.bashrc ~/.bash_aliases
-cp .gitconfig gpg-agent.conf gpg.conf ~/
+cp .gitconfig gpg.conf ~/
+mkdir ~/.gnupg/
+cp gpg-agent.conf ~/.gnupg/gpg-agent.conf
 
 sudo apt-get install -y gnupg2 gnupg-agent scdaemon pcscd
 
