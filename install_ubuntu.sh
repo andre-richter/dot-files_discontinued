73dfcc6925f24709746fdbfbe198131ce4775f6e
Andre Richter <andre.o.richter@gmail.com>
2017-05-30 10:21:14 +0200

    Need curl for installing zsh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install fonts-powerline zsh
+sudo apt install fonts-powerline zsh curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
