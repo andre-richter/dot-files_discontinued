d020d5a1cfd787a592c21761e74fc9845e4faeb9
Andre Richter <andre.o.richter@gmail.com>
2018-03-05 10:22:26 +0100

    Add optional sorting to dtdiff

--- scripts/dtdiff.zsh
+++ scripts/dtdiff.zsh
@@ -24,24 +24,31 @@ function source_and_sort {
 
     if [ -z "$IFORMAT" ]; then
 	echo "Unrecognized format for $DT" >&2
-	exit 2
+	return 2
     fi
 
     OUT=/tmp/$2/$(strip_path_and_extension $DT).dts
-    $DTC -I $IFORMAT -O dts -o "$OUT" "$DT"
+    $DTC $3 -I $IFORMAT -O dts -o "$OUT" "$DT"
 
     echo "$OUT"
 }
 
 function dtdiff {
+
+    if [ "$1" = "-s" ]; then
+	echo "sorting enabled"
+	DTCARGS="-s"
+	shift
+    fi
+
     mkdir -p /tmp/1 /tmp/2
 
     if [ -n "$1" ]; then
-	FIRST=$(source_and_sort $1 "1")
+	FIRST=$(source_and_sort $1 "1" $DTCARGS)
     fi
 
     if [ -n "$2" ]; then
-    	SECOND=$(source_and_sort $2 "2")
+    	SECOND=$(source_and_sort $2 "2" $DTCARGS)
     fi
 
     if [ $# -gt 1 ]; then
