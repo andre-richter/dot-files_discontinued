15a6f20a26d0b30090e8775f9f497747e5061893
Andre Richter <andre.o.richter@gmail.com>
2018-06-28 16:00:38 +0200

    compile back to dtb

--- scripts/dtdiff.zsh
+++ scripts/dtdiff.zsh
@@ -7,6 +7,17 @@ function strip_path_and_extension {
     echo "$BASE"
 }
 
+function is_dtb {
+    BASE=$(basename $1)
+    EXT=${BASE##*.}
+
+    if [ "$EXT" = "dtb" ] ; then
+	echo "true"
+    else
+	echo "false"
+    fi
+}
+
 function source_and_sort {
     DT="$1"
     if [ -d "$DT" ]; then
@@ -39,6 +50,8 @@ function dtdiff {
 	echo "sorting enabled"
 	DTCARGS="-s"
 	shift
+    else
+	DTCARGS=""
     fi
 
     mkdir -p /tmp/1 /tmp/2
@@ -55,5 +68,21 @@ function dtdiff {
     	meld $FIRST $SECOND
     elif [ $# -eq 1 ]; then
     	emacs -nw $FIRST
+    fi
+
+    if [ $? -eq 0 ] && [ -z $DTCARGS ]; then
+	if [ $(is_dtb $1) = "true" ]; then
+	    if [ $# -gt 0 ]; then
+		echo "compiled $FIRST ----> $1"
+		$DTC -I dts -O dtb -o $1 $FIRST
+	    fi
+	    if [ $# -gt 1 ]; then
+		echo "compiled $SECOND ----> $2"
+		$DTC -I dts -O dtb -o $2 $SECOND
+	    fi
+	fi
+    else
+	echo "Your editing tools did not exit gracefully or you had sorting enabled."
+	echo "dtb files were not updated!"
     fi
 }
